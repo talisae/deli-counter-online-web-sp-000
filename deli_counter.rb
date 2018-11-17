da3ad87286caa5e8f8ac0215012b133c3e9ede2c
Talisa Erazo <talisaerazo@gmail.com>
2018-11-17 05:43:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  while katz_deli.length == 0
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else katz_deli.length > 0
     katz_deli.shift

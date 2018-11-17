eeda4c8ddfc3f84f2b6a2b020d424ec35567eb4f
Talisa Erazo <talisaerazo@gmail.com>
2018-11-17 05:50:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,11 +15,9 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(katz_deli, name)
+def now_serving(katz_deli)
   if katz_deli.length == 0
-    puts "There is nobody waiting to be served!"
-  else katz_deli.length > 0
-    katz_deli.shift
-    puts "Currently serving #{name}."
-  end
-end
+      puts "There is nobody waiting to be served!"
+    else
+      puts "Currently serving #{katz_deli.shift}."
+    end

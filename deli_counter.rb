34fd13151ea62b08096e8f72ee755fe097ff22a7
Talisa Erazo <talisaerazo@gmail.com>
2018-11-17 05:29:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     message = "The line is currently:"
-
     katz_deli.each_with_index do |value, index|
-      message += " #{index.to_i+1}. #{value}"
+      message += " #{index.to_i + 1}. #{value}"
     end
     puts "#{message}"
   end
